4489c368344b5aed8335e13098a8b22788cf5c6a
Timtohy <47382986+Timtohy@users.noreply.github.com>
2019-05-20 09:30:36 +0200

    verder opdracht 1

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,1 +1,4 @@
 USE `lo8e_sql2`;
+@ -5,4 +5,17 @@ INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES
+ ('lo9e-amo3', 'rs001', 2018, 'amo'),
+ ('lo9e-amo4', 'vlt03', 2018, 'amo');
